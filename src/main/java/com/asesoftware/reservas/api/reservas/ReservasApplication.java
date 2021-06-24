6bda2a05493aceecd53f750a13d39b2e23c92b06
spinilla <spinilla@asesoftware.com>
2021-06-24 15:05:00 -0500

    Proyecto base

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/ReservasApplication.java
@@ -0,0 +1,17 @@
+package com.asesoftware.reservas.api.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@ConfigurationProperties
+@PropertySource(value = {"classpath:application.properties","file:C://properties//reservas.properties"})
+public class ReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasApplication.class, args);
+	}
+
+}
