3c7e4a1f7e13fc75838c0709495edd7552d23aaa
spinilla <spinilla@asesoftware.com>
2021-06-28 16:59:34 -0500

    Create UsuarioBloqueadoEntity.java

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/entity/UsuarioBloqueadoEntity.java
@@ -0,0 +1,33 @@
+package com.asesoftware.reservas.api.reservas.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import lombok.Data;
+
+
+/**
+ * The persistent class for the USUARIO_ADMINISTRADOR database table.
+ * 
+ */
+@Data
+@Entity
+@Table(name="USUARIO_BLOQUEADO")
+public class UsuarioBloqueadoEntity implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="SEQ_USUARIO_BLOQUEADO", sequenceName="SEQ_USUARIO_BLOQUEADO", allocationSize = 1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_USUARIO_BLOQUEADO")
+	@Column(name="ID_BLOQUEADO")
+	private Integer idBloqueado;
+
+	@Column(name="EMAIL")
+	private String email;
+
+	@Column(name="ID_SUCURSAL")
+	private Integer idSucursal;
+
+	
+
+}
