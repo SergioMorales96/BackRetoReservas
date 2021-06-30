fd2de33347241a0442c0f1ca9c00c9dfd438bcbd
kpinilla <kpinilla@asesoftware.com>
2021-06-30 12:06:58 -0500

    modificacion DTO

--- src/main/java/com/asesoftware/reservas/api/reservas/controller/PuestoTrabajoController.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/controller/PuestoTrabajoController.java
@@ -1,5 +1,10 @@
 package com.asesoftware.reservas.api.reservas.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/v1/puestoTrabajo")
 public class PuestoTrabajoController {
 
 }
--- src/main/java/com/asesoftware/reservas/api/reservas/dto/PuestoTrabajoDTO.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/dto/PuestoTrabajoDTO.java
@@ -1,5 +1,19 @@
 package com.asesoftware.reservas.api.reservas.dto;
 
+import lombok.Data;
+
+@Data
 public class PuestoTrabajoDTO {
+
+	
+	private Integer idPuestoTrabajo;
+	
+	private String dominioEstado;	
+	
+	private String dominioTipo;
+	
+	private Integer idPiso;
+	
+	private String nombre;
 
 }
