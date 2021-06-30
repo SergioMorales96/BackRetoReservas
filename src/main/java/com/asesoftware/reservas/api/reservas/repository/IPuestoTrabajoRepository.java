9d51954d25675cf90b04e21e52a47cb216a8b9dc
kpinilla <kpinilla@asesoftware.com>
2021-06-30 12:12:02 -0500

    otras modificaciones

--- src/main/java/com/asesoftware/reservas/api/reservas/repository/IPuestoTrabajoRepository.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/repository/IPuestoTrabajoRepository.java
@@ -1,5 +1,11 @@
 package com.asesoftware.reservas.api.reservas.repository;
 
-public interface IPuestoTrabajoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.asesoftware.reservas.api.reservas.entity.PuestoTrabajoEntity;
+
+
+
+public interface IPuestoTrabajoRepository extends JpaRepository<PuestoTrabajoEntity, Integer>{
 
 }
