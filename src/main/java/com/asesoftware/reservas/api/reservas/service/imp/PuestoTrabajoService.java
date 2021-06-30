cf4b9a7e56dedfb5e454c61a983256b1cf92313d
kpinilla <kpinilla@asesoftware.com>
2021-06-30 13:10:03 -0500

    otras modificaciones

--- src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
@@ -1,10 +1,17 @@
 package com.asesoftware.reservas.api.reservas.service.imp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import com.asesoftware.reservas.api.reservas.dto.PuestoTrabajoDTO;
 import com.asesoftware.reservas.api.reservas.dto.ResponseDTO;
+import com.asesoftware.reservas.api.reservas.entity.EmpresaEntity;
+import com.asesoftware.reservas.api.reservas.entity.PuestoTrabajoEntity;
+import com.asesoftware.reservas.api.reservas.mapper.IPuestoTrabajoMapper;
 import com.asesoftware.reservas.api.reservas.repository.IPuestoTrabajoRepository;
 import com.asesoftware.reservas.api.reservas.service.IPuestoTrabajoService;
 
@@ -12,18 +19,30 @@ import com.asesoftware.reservas.api.reservas.service.IPuestoTrabajoService;
 public class PuestoTrabajoService implements IPuestoTrabajoService{
 	
 	@Autowired
-	private IPuestoTrabajoRepository puestoTrabajoRepository;
+	private IPuestoTrabajoRepository puestoTrabajoRepositorio;
+	
+	@Autowired
+	private IPuestoTrabajoMapper puestoTrabajoMapper;
 
 	@Override
 	public ResponseDTO consultarTodas() {
-
-		return null;
+		
+	//	logger.info("consultarTodas()");
+		List<PuestoTrabajoEntity> lisPuestoTrabajo = puestoTrabajoRepositorio.findAll();
+	//	Logger.info("consultarTodas() {}",lisPuestoTrabajo);
+		return new ResponseDTO(puestoTrabajoMapper.entitysToDtos(lisPuestoTrabajo), true, "Ok", HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseDTO PuestoTrabajoPorId(Integer idPuestoTrabajo) {
-
-		return null;
+		
+		Optional <PuestoTrabajoEntity> puestoTrabajoEntity = puestoTrabajoRepositorio.findById(idPuestoTrabajo);
+		
+		if(puestoTrabajoEntity.isPresent()) {
+			return new ResponseDTO(puestoTrabajoMapper.entityToDto(puestoTrabajoEntity.get()), true, "OK", HttpStatus.OK);
+		}else {
+			return new ResponseDTO(null, false, "Empresa no disponible", HttpStatus.OK);
+		}
 	}
 
 	@Override
