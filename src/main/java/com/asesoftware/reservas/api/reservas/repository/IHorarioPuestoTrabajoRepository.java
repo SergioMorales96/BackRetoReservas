13d217f216eb697b641f27382f17d18ba22478ac
Jorge Andrés Lucero Hernández <jlucero@asesoftware.com>
2021-06-29 19:22:49 -0500

    Initial Commit
    Se crearon las clases del Servicie, IService, Repository y Controller.

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/repository/IHorarioPuestoTrabajoRepository.java
@@ -0,0 +1,9 @@
+package com.asesoftware.reservas.api.reservas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.asesoftware.reservas.api.reservas.entity.HorarioPuestoTrabajoEntity;
+
+public interface IHorarioPuestoTrabajoRepository extends JpaRepository<HorarioPuestoTrabajoEntity, String> {
+
+}
