e389cd71fa4fa191f1343fcee0f9e27ad4b0287c
kpinilla <kpinilla@asesoftware.com>
2021-06-30 12:50:52 -0500

    otras modificaciones

--- src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
@@ -1,41 +1,46 @@
 package com.asesoftware.reservas.api.reservas.service.imp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.asesoftware.reservas.api.reservas.dto.PuestoTrabajoDTO;
 import com.asesoftware.reservas.api.reservas.dto.ResponseDTO;
+import com.asesoftware.reservas.api.reservas.repository.IPuestoTrabajoRepository;
 import com.asesoftware.reservas.api.reservas.service.IPuestoTrabajoService;
 
 @Service
 public class PuestoTrabajoService implements IPuestoTrabajoService{
+	
+	@Autowired
+	private IPuestoTrabajoRepository puestoTrabajoRepository;
 
 	@Override
 	public ResponseDTO consultarTodas() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ResponseDTO PuestoTrabajoPorId(Integer idPuestoTrabajo) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ResponseDTO crearPuestoTrabajo(PuestoTrabajoDTO puestoTrabajoDTO) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ResponseDTO actualizarPuestoTrabajo(PuestoTrabajoDTO puestoTrabajoDTO) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ResponseDTO borrarPuestoTrabajo(Integer idPuestoTrabajo) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
