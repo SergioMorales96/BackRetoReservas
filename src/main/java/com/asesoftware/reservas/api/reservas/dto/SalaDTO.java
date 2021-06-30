b56934ce3c29214820d256a1c959ed96758a2102
Fabian Esteban Peña Castillo <fpena@asesoftware.com>
2021-06-30 16:44:45 -0500

    Adición de las clases e interfaces de Sala

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/dto/SalaDTO.java
@@ -0,0 +1,16 @@
+package com.asesoftware.reservas.api.reservas.dto;
+
+
+public class SalaDTO {
+	
+	private Integer idSala;
+
+	private Integer aforoMax;
+
+	private String dominioEstado;
+
+	private Integer idPiso;
+
+	private String nombre;
+
+}
---
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/imp/SalaService.java
@@ -0,0 +1,7 @@
+package com.asesoftware.reservas.api.reservas.service.imp;
+
+import com.asesoftware.reservas.api.reservas.service.ISalaService;
+
+public class SalaService implements ISalaService{
+
+}
