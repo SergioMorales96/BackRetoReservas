e9bc9c980877c71d0d6dd78bb147e44ba244ef35
Juan Rondon <84740258+jrondon-asesoftware@users.noreply.github.com>
2021-06-30 11:24:29 -0500

    Implementacion de los atributos y métodos de la clase mapper y dto
    Se crean los atributos y metodos basicos para el funcionamiento de las clases mapper y dto

--- src/main/java/com/asesoftware/reservas/api/reservas/dto/HorarioPuestoTrabajoDTO.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/dto/HorarioPuestoTrabajoDTO.java
@@ -1,5 +1,28 @@
 package com.asesoftware.reservas.api.reservas.dto;
 
+import java.util.Date;
+
+import lombok.Data;
+
+/**
+ * Esta clase contiene la información expuesta en el api-rest para la entidad
+ * horario_puesto_trabajo
+ * 
+ * @author jrondon
+ *
+ */
+@Data
 public class HorarioPuestoTrabajoDTO {
 
+	// VARIABLES DE LA ENTIDAD
+	private Integer idHorario;
+	private Integer idSucursal;
+	private Integer numeroHoras;
+	private String nombre;
+	private Date horaFin;
+	private Date horaInicio;
+
+	// VARIABLES LOGICAS
+	// Anotar aca las variables que no hacen parte de la entidad pero se requieren
+	// para la logica de esta clase
 }
--- src/main/java/com/asesoftware/reservas/api/reservas/mapper/IHorarioPuestoTrabajoMapper.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/mapper/IHorarioPuestoTrabajoMapper.java
@@ -1,5 +1,26 @@
 package com.asesoftware.reservas.api.reservas.mapper;
 
+import java.util.List;
+
+import com.asesoftware.reservas.api.reservas.dto.HorarioPuestoTrabajoDTO;
+import com.asesoftware.reservas.api.reservas.entity.HorarioPuestoTrabajoEntity;
+
+/**
+ * Esta interfaz contiene metodos para castear diferentes arreglos de objetos de
+ * la clase HorarioPuestoTrabajoEntity a la clase HorarioPuestoTrabajoDto o
+ * viceversa
+ * 
+ * @author jrondon
+ *
+ */
 public interface IHorarioPuestoTrabajoMapper {
+
+	public HorarioPuestoTrabajoEntity dtoToEntity(HorarioPuestoTrabajoDTO dto);
+
+	public HorarioPuestoTrabajoDTO entityToDto(HorarioPuestoTrabajoEntity entity);
+
+	public List<HorarioPuestoTrabajoEntity> dtosToEntitys(List<HorarioPuestoTrabajoDTO> dtos);
+
+	public List<HorarioPuestoTrabajoDTO> entitysToDtos(List<HorarioPuestoTrabajoEntity> entitys);
 
 }
