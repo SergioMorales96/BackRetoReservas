13750cb740b07fc976af1b52e3fbc7544838eb22
Daniel Lopez <danielrlopezj@gmail.com>
2021-07-01 17:08:00 -0500

    inicio comentarios y error

--- src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/imp/PuestoTrabajoService.java
@@ -14,32 +14,37 @@ import com.asesoftware.reservas.api.reservas.mapper.IPuestoTrabajoMapper;
 import com.asesoftware.reservas.api.reservas.repository.IPuestoTrabajoRepository;
 import com.asesoftware.reservas.api.reservas.service.IPuestoTrabajoService;
 
+import static com.asesoftware.reservas.api.reservas.utils.Constantes.OK;
+import static com.asesoftware.reservas.api.reservas.utils.Constantes.ERROR_GENERICO;;
+
 @Service
 public class PuestoTrabajoService implements IPuestoTrabajoService{
-	
+
 	@Autowired
 	private IPuestoTrabajoRepository puestoTrabajoRepositorio;
-	
+
 	@Autowired
 	private IPuestoTrabajoMapper puestoTrabajoMapper;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(PuestoTrabajoService.class);
+
+
 
 	@Override
 	public ResponseDTO consultarTodas() {
-		
+
 		logger.info("consultarTodas()");
 		List<PuestoTrabajoEntity> lisPuestoTrabajo = puestoTrabajoRepositorio.findAll();
 		logger.info("consultarTodas() {}", lisPuestoTrabajo);
-		return new ResponseDTO(puestoTrabajoMapper.entitysToDtos(lisPuestoTrabajo), true, "Ok", HttpStatus.OK);
+		return new ResponseDTO(puestoTrabajoMapper.entitysToDtos(lisPuestoTrabajo), true, OK, HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseDTO puestoTrabajoPorId(Integer idPuestoTrabajo) {
-		
+
 		logger.info("ingreso al metodo puestoTrabajoPorId {} ",idPuestoTrabajo );
 		Optional <PuestoTrabajoEntity> puestoTrabajoEntity = puestoTrabajoRepositorio.findById(idPuestoTrabajo);
-		
+
 		if(puestoTrabajoEntity.isPresent()) {
 			return new ResponseDTO(puestoTrabajoMapper.entityToDto(puestoTrabajoEntity.get()), true, "OK", HttpStatus.OK);
 		}else {
@@ -47,29 +52,34 @@ public class PuestoTrabajoService implements IPuestoTrabajoService{
 		}
 	}
 
+	/**
+	 * metodo encargado de crear puesto de trabajo
+	 * @author: Mario R. Rancel
+	 * @version: 22/09/2016/A
+	 */
 	@Override
 	public ResponseDTO crearPuestoTrabajo(PuestoTrabajoDTO puestoTrabajoDTO) {
-		
+
 		logger.info("ingreso al metodo crearPuestoTrabajo {} ",puestoTrabajoDTO );
-		
+
 		try {
 			PuestoTrabajoEntity puestoTrabajoEntity = puestoTrabajoMapper.dtoToEntity(puestoTrabajoDTO);
 			puestoTrabajoRepositorio.save(puestoTrabajoEntity);
 			logger.info("se creo PuestoTrabajo {} ",puestoTrabajoDTO );
-			
+
 			return new ResponseDTO(puestoTrabajoMapper.entityToDto(puestoTrabajoEntity),true, "OK", HttpStatus.OK);
 		}catch (Exception e) {
 			logger.error("Error crear {}",e.getMessage());
 			return new ResponseDTO(null, false, "Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			
+
 		}
 	}
 
 	@Override
 	public ResponseDTO actualizarPuestoTrabajo(PuestoTrabajoDTO puestoTrabajoDTO) {
-		
+
 		logger.info("ingreso al metodo actualizarPuestoTrabajo  ");
-		
+
 		try {
 			puestoTrabajoRepositorio.save(puestoTrabajoMapper.dtoToEntity(puestoTrabajoDTO));
 			logger.info("se actualiza PuestoTrabajo {} ",puestoTrabajoDTO );
@@ -77,27 +87,27 @@ public class PuestoTrabajoService implements IPuestoTrabajoService{
 		}catch (Exception e) {
 			logger.error("Error {}",e.getMessage());
 			return new ResponseDTO(null, false, "Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			
+
 		}
 
-		
+
 	}
 
 	@Override
 	public ResponseDTO borrarPuestoTrabajo(Integer idPuestoTrabajo) {
 
 		logger.info("ingreso al metodo borrarPuestoTrabajo ");
-		
+
 		try {
 			puestoTrabajoRepositorio.deleteById(idPuestoTrabajo);
-			
+
 			logger.info("El puesto de trabajo {} se elimino",idPuestoTrabajo);
-			
+
 			return  new ResponseDTO(null, true, "Puesto de trabajo eliminado", HttpStatus.OK); 
 		}catch (Exception e) {
-			
+
 			logger.error("Error {}",e.getMessage());
-			
+
 			return  new ResponseDTO(null, false, "El puesto de trabajo no se puede eliminar", HttpStatus.OK); 
 		}
 	}
