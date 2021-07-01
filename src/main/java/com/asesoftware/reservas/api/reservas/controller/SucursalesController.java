b2ba70987c6122e80743162e9ff2c7674ae63b02
smmorales <smmorales@asesoftware.com>
2021-07-01 14:55:14 -0500

    Creación clase SucursalesController

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/controller/SucursalesController.java
@@ -0,0 +1,14 @@
+package com.asesoftware.reservas.api.reservas.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/sucursales")
+public class SucursalesController {
+	
+	@Autowired
+	private ISucursalesService sucursalesService;
+
+}
