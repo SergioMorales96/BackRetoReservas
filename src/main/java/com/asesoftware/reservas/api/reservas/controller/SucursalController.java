9694720ad39fb9609b1cccda2f3d2d53491b19fe
smmorales <smmorales@asesoftware.com>
2021-07-01 17:11:18 -0500

    Creación de servicios para Crear y Editar sucursales

--- src/main/java/com/asesoftware/reservas/api/reservas/controller/SucursalController.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/controller/SucursalController.java
@@ -1,9 +1,12 @@
 package com.asesoftware.reservas.api.reservas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.asesoftware.reservas.api.reservas.dto.ResponseDTO;
+import com.asesoftware.reservas.api.reservas.dto.SucursalDTO;
 import com.asesoftware.reservas.api.reservas.service.ISucursalService;
 
 @RestController
@@ -11,6 +14,23 @@ import com.asesoftware.reservas.api.reservas.service.ISucursalService;
 public class SucursalController {
 	
 	@Autowired
-	private ISucursalService sucursalesService;
+	private ISucursalService sucursalService;
+	
+	
+	// Crear 
+	
+		@PostMapping(path = "/crear", consumes = "application/json", produces = "application/json")
+		public ResponseDTO createSucursal(@RequestBody SucursalDTO entity) {
+						
+			return sucursalService.createSucursal(entity);
+		}
+		
+		// Editar
+		
+		@PostMapping(path = "/editar", consumes = "application/json", produces = "application/json")
+		public ResponseDTO editarSucursal(@RequestBody SucursalDTO entity) {
+						
+			return sucursalService.updateSucursal(entity);
+		}
 
 }
--- src/main/java/com/asesoftware/reservas/api/reservas/service/ISucursalService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/ISucursalService.java
@@ -1,8 +1,12 @@
 package com.asesoftware.reservas.api.reservas.service;
-
-
+import com.asesoftware.reservas.api.reservas.dto.ResponseDTO;
+import com.asesoftware.reservas.api.reservas.dto.SucursalDTO;
 
 public interface ISucursalService {
+
+	public ResponseDTO createSucursal(SucursalDTO sucursalDTO);
+
+	public ResponseDTO updateSucursal(SucursalDTO sucursalDTO);
 
 
 }
