851ba1ce0bae16b4c3f936a3ccb38dd3481d6695
WilliamSL14 <wesierral@correo.udistrital.edu.co>
2021-07-01 15:15:00 -0500

    Correcion query y adicion logger a servicio
    Se corrigió el query en el IUsuarioAdministradorRepository y se agregó el logger para el UsuarioAdministradorService

--- src/main/java/com/asesoftware/reservas/api/reservas/repository/IUsuarioAdministradorRepository.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/repository/IUsuarioAdministradorRepository.java
@@ -4,13 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.asesoftware.reservas.api.reservas.entity.UsuarioAdministradorEntity;
 
 public interface IUsuarioAdministradorRepository extends JpaRepository<UsuarioAdministradorEntity, Integer>{
 	
-	@Query(nativeQuery = true , value = "Select * from USUARIO_ADMINISTRADOR where id_sucursal = :id")
-	List<UsuarioAdministradorEntity> queryAdminPorSucursal(@Param("id") Integer idSucursal);
+	@Query("SELECT t from UsuarioAdministradorEntity t WHERE t.idSucursal = ?1")
+	List<UsuarioAdministradorEntity> queryAdminPorSucursal(Integer idSucursal);
 
 }
--- src/main/java/com/asesoftware/reservas/api/reservas/service/UsuarioAdministradorService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/UsuarioAdministradorService.java
@@ -2,6 +2,8 @@ package com.asesoftware.reservas.api.reservas.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ import com.asesoftware.reservas.api.reservas.repository.IUsuarioAdministradorRep
 @Service
 public class UsuarioAdministradorService implements IUsuarioAdministradorService{
 	
+	private static final Logger logger = LoggerFactory.getLogger(UsuarioAdministradorService.class);
+	
 	@Autowired
 	private IUsuarioAdministradorRepository usuarioAdminRepository;
 	
@@ -23,9 +27,13 @@ public class UsuarioAdministradorService implements IUsuarioAdministradorService
 	@Override
 	public ResponseDTO adminPorSucursal(Integer idSucursal) {
 		
+		logger.info("ingreso al método adminPorSurcursal()");
+		
 		List<UsuarioAdministradorDTO> listUsuarioAdministradorSucursal = usuarioAdminMapper.entitysToDtos(usuarioAdminRepository.queryAdminPorSucursal(idSucursal));
 			
-		return new ResponseDTO(listUsuarioAdministradorSucursal, true, "Ok", HttpStatus.OK);
+		logger.info("Se realizó busqueda de administradores de la sucursal {}", idSucursal);
+		
+		return new ResponseDTO(listUsuarioAdministradorSucursal, true, "Consulta con éxito", HttpStatus.OK);
 			
 	}
 
