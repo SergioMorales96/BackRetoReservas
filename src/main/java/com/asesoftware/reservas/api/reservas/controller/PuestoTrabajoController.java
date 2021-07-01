fa3b9e52e1e48f995eefc589174f03ded918fb20
kpinilla <kpinilla@asesoftware.com>
2021-07-01 15:11:06 -0500

    arreglo consultar id

--- src/main/java/com/asesoftware/reservas/api/reservas/controller/PuestoTrabajoController.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/controller/PuestoTrabajoController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class PuestoTrabajoController {
 	}
 	
 	@GetMapping(path = "/{idPuestoTrabajo}")
-	public ResponseDTO puestoTrabajoPorId(Integer idPuestoTrabajo) {
+	public ResponseDTO puestoTrabajoPorId(@PathVariable Integer idPuestoTrabajo) {
 		
 		logger.info("puestoTrabajoPorId: {}", idPuestoTrabajo);
 		
