80de4ecc4b9e3896f984e9a77b4457638e5a70d3
abarrios0209 <80713264+abarrios0209@users.noreply.github.com>
2021-07-01 16:59:06 -0500

    IDominiosService

--- src/main/java/com/asesoftware/reservas/api/reservas/service/IDominioService.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/service/IDominioService.java
@@ -1,5 +1,15 @@
 package com.asesoftware.reservas.api.reservas.service;
 
-public interface IDominioService {
+import com.asesoftware.reservas.api.reservas.dto.DominioDTO;
+import com.asesoftware.reservas.api.reservas.dto.ResponseDTO;
 
+public interface IDominioService {
+	
+	public ResponseDTO createDominio(DominioDTO dominioDTO);
+	
+	public ResponseDTO reloadDominio(DominioDTO dominioDTO);
+	
+	public ResponseDTO updateDominio(DominioDTO dominioDTO);
+	
+	public ResponseDTO deleteDominio(String dominioPK);
 }
