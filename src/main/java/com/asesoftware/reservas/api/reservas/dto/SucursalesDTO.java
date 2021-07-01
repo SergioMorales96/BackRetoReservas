a20c8ee69ad4837deec7bf37f79d6cb024fd2596
Andrea Moya <moya.andrea@javeriana.edu.co>
2021-07-01 14:44:01 -0500

    Creacion SucursalesDTO

---
+++ src/main/java/com/asesoftware/reservas/api/reservas/dto/SucursalesDTO.java
@@ -0,0 +1,18 @@
+package com.asesoftware.reservas.api.reservas.dto;
+
+import lombok.Data;
+
+@Data
+public class SucursalesDTO {
+	
+	private Integer idSucursal;
+	
+	private Integer aforoMaximo;
+	
+	private String direccion;
+	
+	private String nit;
+	
+	private String nombre;
+
+}
