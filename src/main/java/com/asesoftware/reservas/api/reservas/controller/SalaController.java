3f7ee9345cdb3f75943170431e61558cf40c2100
Fabian Esteban Peña Castillo <fpena@asesoftware.com>
2021-07-01 08:58:45 -0500

    Adición de método de actualizar y eliminar una sala

--- src/main/java/com/asesoftware/reservas/api/reservas/controller/SalaController.java
+++ src/main/java/com/asesoftware/reservas/api/reservas/controller/SalaController.java
@@ -40,12 +40,18 @@ public class SalaController {
 		return null;
 	}
 	
-	public ResponseDTO actualizarSala(){
-		return null;
+	@PostMapping(path = "/actualizar", consumes = "application/json", produces = "application/json")
+	public ResponseDTO actualizarSala(@RequestBody SalaDTO salaDTO){
+		logger.info("Sala para actualizar: {}", salaDTO);
+		return salaService.actualizarSala(salaDTO);
+
 	}
 	
-	public ResponseDTO eliminarSala() {
-		return null;
+	@GetMapping(path = "/eliminar/{idSala}")
+	public ResponseDTO eliminarSala(@PathVariable Integer idSala) {
+		logger.info("Sala a eliminar: {}", idSala);
+		return salaService.eliminarSala(idSala);
+
 	}
 	
 }
